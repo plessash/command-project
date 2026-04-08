ef6d02a18a611801d29549c6ba1726d6b284f75d
alexanderganych <alexander.ganych173@gmail.com>
2026-04-08 13:37:17 +0300

    feat/KAN-11 additional task 1

--- src/main/java/ru/aston/service/CarManager.java
+++ src/main/java/ru/aston/service/CarManager.java
@@ -2,8 +2,10 @@ package ru.aston.service;
 
 import ru.aston.model.Car;
 import ru.aston.service.strategy.CarFillStrategy;
+import ru.aston.service.strategy.SortStrategy;
 
 import java.util.List;
+import java.util.Comparator;
 
 public class CarManager {
     private CarFillStrategy strategy;
@@ -15,5 +17,10 @@ public class CarManager {
     public List<Car> fillData(int count) {
         if (strategy == null) throw new IllegalStateException("Стратегия не выбрана!");
         return strategy.fill(count);
+    }
+
+    public void sortCars(List<Car> cars, SortStrategy<Car> strategy, Comparator<Car> comparator) {
+        if (strategy == null) throw new IllegalStateException("Стратегия сортировки не выбрана!");
+        strategy.sort(cars, comparator);
     }
 }
